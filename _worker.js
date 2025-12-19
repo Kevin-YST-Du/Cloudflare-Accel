38a3461b5214b1522866a7dc46d5a20d9a6b9f69
Kevin-YST-Du <143725301+Kevin-YST-Du@users.noreply.github.com>
2025-12-20 01:48:14 +0800

    更新

--- _worker.js
+++ _worker.js
@@ -775,7 +775,7 @@ const DEFAULT_CONFIG = {
         </div>
         <p class="text-[11px] opacity-60 flex items-center gap-1">
           <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-          失败自动退还额度 · 短时重复请求不扣费。（10S）
+          失败自动退还额度 · 短时重复请求不扣费。（10s）
         </p>
       </div>
   
